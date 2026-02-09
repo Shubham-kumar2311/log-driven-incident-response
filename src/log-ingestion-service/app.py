48b51232368b1e22d8f99da780254682c7721a6f
Shubham-kumar2311 <shubhamkumar231107@gmail.com>
2026-02-10 00:01:30 +0530

    corrected app.py

--- src/log-ingestion-service/app.py
+++ src/log-ingestion-service/app.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from ingestion_service import LogIngestionService
 
 app = FastAPI(title="Log Ingestion Service")
 service = LogIngestionService()
 
 @app.post("/ingest")
-def ingest_log(raw_log: str):
+async def ingest_log(request: Request):
+    raw_log = (await request.body()).decode("utf-8")
     return service.ingest(raw_log)
